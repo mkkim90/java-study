aadfdbe3172b70e9ee33adbfa7afc41f0936bee8
mkkim90 <46394672+mkkim90@users.noreply.github.com>
2020-12-15 22:31:39 +0900

    repository create (#2)

---
+++ src/test/java/week4/LinkedListTest.java
@@ -0,0 +1,15 @@
+package week4;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListTest {
+
+    @Test
+    @DisplayName("node 생성")
+    void Node() {
+        Node node = new Node();
+        
+    }
+}
